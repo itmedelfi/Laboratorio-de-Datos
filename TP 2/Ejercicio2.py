75585c6a9ffc38ff5489d3e187d5eadcb895b8d6
ktikg <catherineg0502@gmail.com>
2026-03-02 17:53:48 -0300

    Update Ejercicio2.py

--- TP 2/Ejercicio2.py
+++ TP 2/Ejercicio2.py
@@ -120,53 +120,60 @@ plt.show()
 
 #%% Ejercicio 2: Clasificación binaria
 
+# Fijamos una semilla, para que los números generados al azar sean siempre los mismos, y tener consistencia en el 
+# análisis.
+np.random.seed(42)
+
+
 # Dataframe con solo el subconjunto de imágenes correspondientes a las letras O y L
 df_L_O = df_letras[(df_letras["label"] == 11) | (df_letras["label"] == 14)] 
 
 # Separamos en las imágenes correspondientes a la letra L
-df_L = df_letras[(df_letras["label"] == 11)]
+df_L = df_L_O[df_L_O["label"] == 11]
 
 # Separamos en las imágenes correspondientes a la letra O
-df_O = df_letras[(df_letras["label"] == 14)]
+df_O = df_L_O[df_L_O["label"] == 14]
 
-# Calculamos la media de cada columna
+# Calculamos la media de cada columna(píxel)
 media_L = df_L.iloc[:,1:].mean()
 media_O = df_O.iloc[:,1:].mean()
 
+# Calculamos la varianza de cada columna(píxel)
+var_L = df_L.iloc[:,1:].var()
+var_O = df_O.iloc[:,1:].var()
 
 # Calculamos la diferencia entre la media de las imágenes correspondientes a las letras O y L
 diferencia = media_L - media_O
-diferencia_img = diferencia.values.reshape(28,28)
-interseccion_img = np.minimum(media_L.values, media_O.values).reshape(28,28)
 
-# Graficamos la diferencia usando un mapa de color.
-im = plt.imshow(diferencia_img, cmap='RdBu', vmin=-abs(diferencia).max(), vmax=abs(diferencia).max())
-plt.title("Intensidad Media por Píxel(L-O)", fontsize=14)
-plt.colorbar(im)
-plt.axis('off')
-plt.show()
-
-# Graficamos la interseccion
-plt.figure()
-plt.imshow(interseccion_img, cmap='magma_r')
-plt.title("Intersección de trazos", fontsize=14)
-plt.colorbar(label='Nivel de Intensidad')
-plt.axis('off')
-plt.show()
+# Buscamos píxeles que sean consistentes en dentro de los dataframes, pero que varien entre ellos.
+S = (media_L - media_O)**2 / (var_L + var_O)
 
 
 # Separamos los datos y las etiquetas
 X1 = df_L_O.iloc[:, 1:]
 y1 = df_L_O["label"]
 
+# Generamos nuestros conjuntos de training y testing.
 X_train, X_test, y_train, y_test = train_test_split(
     X1, y1, test_size=0.2, random_state=42)
 
-top12 = diferencia.abs().sort_values(ascending=False).head(12)
+# Creamos distintas listas de indices para utilizar en el training del modelo y comparar sus resultados entre sí.
+top3_dif = diferencia.abs().sort_values(ascending=False).index[:3].tolist()
+top6_dif = diferencia.abs().sort_values(ascending=False).index[:6].tolist()
+top12_dif = diferencia.abs().sort_values(ascending=False).index[:12].tolist()
+top25_dif = diferencia.abs().sort_values(ascending=False).index[:25].tolist()
+top3_fs = S.sort_values(ascending = False).index[:3].tolist()
+top6_fs = S.sort_values(ascending = False).index[:6].tolist()
+top12_fs = S.sort_values(ascending = False).index[:12].tolist()
+top25_fs = S.sort_values(ascending = False).index[:25].tolist()
+numeros_azar = np.random.choice(range(784), size=6, replace=False)
+columnas_azar3 = X_train.columns[numeros_azar[:3]].tolist()
+columnas_azar6 = X_train.columns[numeros_azar[:6]].tolist()
 
+# Función para entrenar el modelo, que tiene como entrada los atributos y el k a utilizar y de salida, la exactitud.
 def ajuste (indices, k):
-    X_tr = X_train.iloc[:,indices]
-    X_ts = X_test.iloc[:,indices]
+    X_tr = X_train[indices]
+    X_ts = X_test[indices]
     
     modelo = KNeighborsClassifier(n_neighbors=k)
     modelo.fit(X_tr, y_train)
@@ -175,17 +182,78 @@ def ajuste (indices, k):
     
     acc = accuracy_score(y_test, y_prd_ts)
     
-    return (f"Atributos usados: {indices}, Accuracy: {acc}")
+    return acc
+
+  
+resultados = []
+
+# Lista con las distintias configuraciones que vamos a usar para entrenar el modelo.
+configuraciones = [
+    ("diferencia_media", 3, top3_dif),
+    ("azar", 3, columnas_azar3),
+    ("fisher_score", 3, top3_fs),
+    ("borde", 3, ['pixel 0', 'pixel 1', 'pixel 2']),
     
+    ("diferencia_media", 6, top6_dif),
+    ("azar", 6, columnas_azar6),
+    ("fisher_score", 6, top6_fs),
+    ("borde", 6, ['pixel 0','pixel 1','pixel 2','pixel 3','pixel 4','pixel 5']),
+    
+    ("diferencia_media", 12, top12_dif),
+    ("fisher_score", 12, top12_fs),
+    
+    ("diferencia_media", 25, top25_dif),
+    ("fisher_score", 25, top25_fs),
+]
 
-# Seleccionamos los siguientes 3 atributos con mayor diferencia, con un k = 3
-print(ajuste([245,273,453], 3))
+valores_k = [3, 5, 25]
 
-print(ajuste([425,358,330], 3))
+# Entrenamos el modelo con las configuraciones y lo agregamos a una lista 'resultados'.
+for criterio, cantidad, columnas in configuraciones:
+    for k in valores_k:
+        acc = ajuste(columnas, k)
+        resultados.append({
+            "criterio": criterio,
+            "n_atributos": cantidad,
+            "k": k,
+            "accuracy": acc
+        })
 
-print(ajuste([245,273,453], 10))
-#%%
+# Pasamos la lista 'resultados' a un dataframe para facilitar su lectura y análisis.
+df_resultados = pd.DataFrame(resultados)
 
+# Vamos a pasar nuestros resultados a unos gráficos.
+orden_atributos = sorted(df_resultados["n_atributos"].unique())
+valores_k = sorted(df_resultados["k"].unique())
+
+for k_val in valores_k:
+    
+    # Filtramos por k
+    df_k = df_resultados[df_resultados["k"] == k_val]
+    
+    plt.figure()
+    
+    # Graficamos una línea por criterio
+    for criterio in df_k["criterio"].unique():
+        
+        df_crit = df_k[df_k["criterio"] == criterio]
+        df_crit = df_crit.sort_values("n_atributos")
+        
+        plt.plot(
+            df_crit["n_atributos"],
+            df_crit["accuracy"],
+            marker='o',
+            label=criterio
+        )
+    
+    plt.xlabel("Cantidad de atributos")
+    plt.ylabel("Accuracy")
+    plt.title(f"Desempeño del modelo KNN (k = {k_val})")
+    plt.xticks(orden_atributos)
+    plt.ylim(0.5, 1)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 #%% --- representacion grafica
 # img = np.array(df_letras.sample()).reshape((28,28))
 # plt.imshow(img, cmap='gray')
